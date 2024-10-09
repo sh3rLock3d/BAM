d4b3be34e4d721f8f0715f570de66f5ca20cf7cb
sh3rlock3d <alipourghasemi78@gmail.com>
2024-10-09 13:33:55 -0400

    Writing the scraper

--- config/scraper_config.py
+++ config/scraper_config.py
@@ -3,7 +3,7 @@
 client_id = "wJUpDRAsZW-qXK-O2HDKiw"
 client_secret = "6FuDkrmmgF9AX3O-IgZFBgZoFEXYrg"
 user_agent="script:ai_comment_extractor:v1.0 (by u/TrainingCamp6579)"
-subreddit_name= 'privacy'
-search_query = 'AI'
+subreddit_names= ['privacy', 'Ethics']
+search_queries = ['AI']
 search_limit=10 # Number of posts we want to extract.
 
--- tools/reddit_scraper.py
+++ tools/reddit_scraper.py
@@ -10,35 +10,35 @@ reddit = praw.Reddit(
     user_agent=user_agent
 )
 
-subreddit = reddit.subreddit(subreddit_name)
 
-posts = subreddit.search(search_query, limit=search_limit)
+for subreddit_name in subreddit_names:
+    for search_query in search_queries:
+        subreddit = reddit.subreddit(subreddit_name)
+        posts = subreddit.search(search_query, limit=search_limit)
 
-posts_data = []
-for post in posts:
-    post_info = {
-        "title": post.title,
-        "score": post.score,
-        "url": post.url,
-        "num_comments": post.num_comments,
-        "comments": []
-    }
+        posts_data = []
+        for post in posts:
+            post_info = {
+                "title": post.title,
+                "selftext": post.selftext,
+                "score": post.score,
+                "url": post.url,
+                "num_comments": post.num_comments,
+                "comments": []
+            }
+            post.comments.replace_more(limit=0)  # Replace "MoreComments" instances
+            for comment in post.comments.list():
+                comment_info = {
+                    "body": comment.body,
+                    "score": comment.score
+                }
+                post_info["comments"].append(comment_info)
 
-    # Extract the comments of the post
-    post.comments.replace_more(limit=0)  # Replace "MoreComments" instances
-    for comment in post.comments.list():
-        comment_info = {
-            "body": comment.body,
-            "score": comment.score
-        }
-        post_info["comments"].append(comment_info)
+            posts_data.append(post_info)
 
-    posts_data.append(post_info)
+        os.makedirs("../data/raw", exist_ok=True)
+        file_path = f"../data/raw/reddit_posts_{subreddit_name}_{search_query}.json"
+        with open(file_path, "w", encoding="utf-8") as f:
+            json.dump(posts_data, f, ensure_ascii=False, indent=4)
 
-# Save the data to JSON file
-os.makedirs("../data/raw", exist_ok=True)
-file_path = "../data/raw/reddit_posts.json"
-with open(file_path, "w", encoding="utf-8") as f:
-    json.dump(posts_data, f, ensure_ascii=False, indent=4)
-
-print(f"Data saved to {file_path}")
+        print(f"Data saved to {file_path}")
